bec3a27f9dc65b2c0fdc0bfbc4a1a0dbac997b25
Bertrand Roussel <broussel@sierrawireless.com>
2017-08-24 22:58:22 -0700

    Update script to fetch + reset (no pull)
    The 'git pull' can provide weird results when there
    was a force merged.
    
    Resolves: FARM-941
    Change-Id: I12a3cc08bbf835b5c7d0d9a3ac958632a0bfdcc2

--- update.sh
+++ update.sh
@@ -1,11 +1,16 @@
 #!/bin/sh
 
+echo "[$(date +"%H:%M:%S")] === Start ==="
+
 source /etc/update.env.sh
 
 cd $VOLUME_PATH
 
 # Get latest changes from upstream
-git pull
+git fetch
 
 # Make sure that there are no changes applied to that repository
-git reset --hard
+git reset --hard FETCH_HEAD
+
+echo -e "[$(date +"%H:%M:%S")] --- End ---\n"
+
